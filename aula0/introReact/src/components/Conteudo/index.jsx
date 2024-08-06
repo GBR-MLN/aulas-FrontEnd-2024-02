3b86b8c870e68694acb4eb90d22c3279d24e0ccb
GABRIEL CAETANO MILANI <gabriel_milani@estudante.sesisenai.org.br>
2024-08-06 17:17:35 -0300

    Criação de páginas e utilização de icones svg

--- aula0/introReact/src/components/Conteudo/index.jsx
+++ aula0/introReact/src/components/Conteudo/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Duck } from "./Duck"
 import './index.css'
 
 function Conteudo() {
@@ -7,6 +8,13 @@ function Conteudo() {
       <p>
         <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi tempora vero dolorem fuga sequi eum sit nisi magni, aliquid cupiditate. Architecto magnam neque dicta dignissimos quo quas repudiandae error nulla?</h1>
       </p>
+      <div className="img-container">
+        <img src="public/imgs/breno.jpg" alt="breno" />
+      </div>
+      <p>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi tempora vero dolorem fuga sequi eum sit nisi magni, aliquid cupiditate. Architecto magnam neque dicta dignissimos quo quas repudiandae error nulla?</h1>
+      </p>
+        <Duck />
     </div>
   );
 }
---
+++ aula0/introReact/src/pages/Home/index.jsx
@@ -0,0 +1,15 @@
+
+import Navbar from "../../components/Navbar"
+import Conteudo from "../../components/Conteudo"
+import './index.css'
+
+function Home() {
+  return (
+    <div className='home-container '>
+      <Navbar />
+      <Conteudo />
+    </div>
+  )
+}
+
+export default Home
